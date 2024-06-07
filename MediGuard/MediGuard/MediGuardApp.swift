245bbacfa21f0c888d5cba8c3c9a01bd9881fdbf
alvaro <adelcastifo@gmail.com>
2024-06-07 06:11:39 +0200

    Organized project structure and added Firebase dependencies
    - Created folders/groups in Xcode for Extensions, Enums, Manager, Models, Views, Repositories, and ViewModels.
    - Added package dependencies for FirebaseAuth, FirebaseDatabase, FirebaseStorage, and Crashlytics.
    - Included GoogleService-Info.plist file.

---
+++ MediGuard/MediGuard/MediGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediGuardApp.swift
+//  MediGuard
+//
+//  Created by Alvaro Guillermo del Castillo Forero on 07.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MediGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
