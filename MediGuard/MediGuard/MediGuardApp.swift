60f694d5f38d436451022e8453a2088f26816212
alvaro <adelcastifo@gmail.com>
2024-06-16 08:31:48 +0200

    Documentation for the @main- struct of the MediGuard App extended and comments added.

--- MediGuard/MediGuard/MediGuardApp.swift
+++ MediGuard/MediGuard/MediGuardApp.swift
@@ -8,29 +8,46 @@
 import SwiftUI
 import Firebase
 
+/**
+ Die `MediGuardApp`-Struktur ist der Haupteinstiegspunkt der App und konfiguriert Firebase bei der Initialisierung.
+ 
+ Diese Struktur entscheidet auch, welche View (`HomeView` oder `AuthenticationView`) basierend auf dem Anmeldestatus des Benutzers angezeigt wird.
+
+ - Eigenschaften:
+    - `userViewModel`: Das `UserViewModel`-Objekt, das die Authentifizierungslogik und -daten verwaltet.
+ */
 @main
 struct MediGuardApp: App {
     
+    // MARK: - Initializer
     
-    init(){
+    /**
+     Initialisiert die App und konfiguriert Firebase.
+     */
+    init() {
         FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
     }
     
+    // MARK: - Variables
+    
+    @StateObject private var userViewModel = UserViewModel()
+    
+    // MARK: - Body
+    
     var body: some Scene {
         WindowGroup {
-            if userViewModel.userIsLoggedIn{
+            // Entscheidet, welche View basierend auf dem Anmeldestatus des Benutzers angezeigt wird
+            if userViewModel.userIsLoggedIn {
+                // Zeigt die HomeView an, wenn der Benutzer angemeldet ist
                 HomeView()
                     .environmentObject(userViewModel)
-            }else{
+            } else {
+                // Zeigt die AuthenticationView an, wenn der Benutzer nicht angemeldet ist
                 AuthenticationView()
                     .environmentObject(userViewModel)
             }
-            
         }
     }
-    
-    // MARK: - Variables
-        
-        @StateObject private var userViewModel = UserViewModel()
 }
+
