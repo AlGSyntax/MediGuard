c74522b6051f2349a779a6a9d9d8f75396872973
alvaro <adelcastifo@gmail.com>
2024-06-16 08:49:14 +0200

    Documentation for the HomeView extended and comments added.

--- MediGuard/MediGuard/Views/Home/View/HomeView.swift
+++ MediGuard/MediGuard/Views/Home/View/HomeView.swift
@@ -7,18 +7,34 @@
 
 import SwiftUI
 
+/**
+ Die `HomeView`-Struktur ist eine SwiftUI-View, die den Hauptbildschirm der App darstellt.
+ 
+ Diese View wird angezeigt, wenn der Benutzer erfolgreich authentifiziert ist und bietet Navigation zu verschiedenen Detailansichten sowie eine Notruffunktion.
+
+ - Eigenschaften:
+    - `homeViewModel`: Das `HomeViewModel`-Objekt, das die Daten und Logik für die `HomeView` verwaltet.
+    - `settingsViewModel`: Das `SettingsViewModel`-Objekt, das die Daten und Logik für die Einstellungen verwaltet.
+    - `userViewModel`: Das `UserViewModel`-Objekt, das die Benutzerdaten verwaltet.
+ */
 struct HomeView: View {
     
     @StateObject private var homeViewModel = HomeViewModel()
     @StateObject private var settingsViewModel = SettingsViewModel()
     @EnvironmentObject private var userViewModel: UserViewModel
     
+    // MARK: - Body
     var body: some View {
         NavigationStack {
             VStack {
+                
+                // MARK: - Begrüßung
+                
                 Text(homeViewModel.greeting)
                     .font(.largeTitle)
                     .padding(.top, 16)
+                
+                // MARK: - Grafisches Element
                 
                 Rectangle()
                     .fill(Color.gray)
@@ -28,6 +44,8 @@ struct HomeView: View {
                             .foregroundColor(.white)
                     )
                     .padding(.vertical, 16)
+                
+                // MARK: - Navigation Links
                 
                 VStack(spacing: 8) {
                     NavigationLink(destination: MedicationDetailView(userViewModel: userViewModel)) {
@@ -43,6 +61,8 @@ struct HomeView: View {
                     }
                 }
                 .padding()
+                
+                // MARK: - Notruf Button
                 
                 EmergencyCallButton(title: "Notruf", action: {
                     homeViewModel.callEmergencyContact()
@@ -65,9 +85,12 @@ struct HomeView: View {
     }
 }
 
+// MARK: - Vorschau
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(UserViewModel())
             .environmentObject(HomeViewModel())
             .environmentObject(SettingsViewModel())
     }
