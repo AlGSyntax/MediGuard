df2fc101a0d64cd775f30441bf224a3e94297347
alvaro <adelcastifo@gmail.com>
2024-06-16 08:57:59 +0200

    Documentation for SettingsViewModel extended and comments added.

--- MediGuard/MediGuard/ViewModels/Settings/ViewModel/SettingsViewModel.swift
+++ MediGuard/MediGuard/ViewModels/Settings/ViewModel/SettingsViewModel.swift
@@ -7,16 +7,39 @@
 
 import SwiftUI
 
+/**
+ Das `SettingsViewModel` verwaltet die Daten und Logik für die Einstellungen der App.
+ 
+ Es enthält Funktionen zum Verwalten und Speichern des Notfallkontakts.
 
+ - Eigenschaften:
+    - `emergencyContact`: Eine Zeichenkette, die die gespeicherte Notfallnummer darstellt.
+ */
 class SettingsViewModel: ObservableObject {
+    
+    // MARK: - Published Properties
+    
     @Published var emergencyContact: String
     
+    // MARK: - Initializer
+    
+    /**
+     Initialisiert das `SettingsViewModel` und lädt den gespeicherten Notfallkontakt aus den `UserDefaults`.
+     
+     Wenn kein Notfallkontakt gespeichert ist, wird die Standardnummer "112" verwendet.
+     */
     init() {
         emergencyContact = UserDefaults.standard.string(forKey: "emergencyContact") ?? "112"
     }
     
+    // MARK: - Functions
+    
+    /**
+     Speichert den Notfallkontakt in den `UserDefaults`.
+     */
     func saveEmergencyContact() {
         UserDefaults.standard.set(emergencyContact, forKey: "emergencyContact")
     }
 }
+
 
