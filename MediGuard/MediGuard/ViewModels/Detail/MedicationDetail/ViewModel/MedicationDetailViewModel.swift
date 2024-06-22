762632aa3abf46dd65dc7073ebfeca9f81814336
alvaro <adelcastifo@gmail.com>
2024-06-22 07:20:11 +0200

    .append replaced by addSnapShotListener

--- MediGuard/MediGuard/ViewModels/Detail/MedicationDetail/ViewModel/MedicationDetailViewModel.swift
+++ MediGuard/MediGuard/ViewModels/Detail/MedicationDetail/ViewModel/MedicationDetailViewModel.swift
@@ -7,46 +7,59 @@
 
 import SwiftUI
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 // MARK: - MedicationDetailViewModel Klasse
 
 /**
- Die `MedicationDetailViewModel`-Klasse verwaltet die Daten und die Logik für die Detailansicht von Medikamenten.
+ Die MedicationDetailViewModel-Klasse verwaltet die Daten und die Logik für die Detailansicht von Medikamenten.
  
  Diese Klasse ermöglicht das Abrufen, Hinzufügen, Aktualisieren und Löschen von Medikamenten sowie das Planen von Benachrichtigungen.
  */
 @MainActor
 class MedicationDetailViewModel: ObservableObject {
     @Published var medications: [Medication] = []
+    
+    private var listener: ListenerRegistration?
 
     // MARK: - Methoden
     
-    init(){
+    init() {
         listScheduledNotifications()
     }
-    
 
     /**
-     Ruft die Medikamente für den angegebenen Benutzer aus der Firestore-Datenbank ab.
+     Fügt einen Listener hinzu, der die Medikamente für den angegebenen Benutzer aus der Firestore-Datenbank in Echtzeit abruft.
      
      - Parameter userId: Die eindeutige Kennung des Benutzers.
      */
     func fetchMedications(userId: String) {
-        Firestore.firestore().collection("users").document(userId).collection("medications").getDocuments { (snapshot, error) in
-            if let error = error {
-                print("Error fetching medications: \(error)")
-                return
+        listener?.remove() // Entfernt den alten Listener, falls vorhanden
+        
+        listener = Firestore.firestore().collection("users").document(userId).collection("medications")
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error fetching medications: \(error)")
+                    return
+                }
+                
+                guard let documents = querySnapshot?.documents else {
+                    print("No medications found")
+                    return
+                }
+                
+                self.medications = documents.compactMap { doc -> Medication? in
+                    try? doc.data(as: Medication.self)
+                }
             }
-
-            guard let documents = snapshot?.documents else {
-                print("No medications found")
-                return
-            }
-
-            self.medications = documents.compactMap { doc -> Medication? in
-                try? doc.data(as: Medication.self)
-            }
-        }
+    }
+    
+    /**
+     Entfernt den Listener, um Änderungen zu stoppen.
+     */
+    func removeListener() {
+        medications.removeAll()
+        listener?.remove()
     }
 
     /**
@@ -59,7 +72,6 @@ class MedicationDetailViewModel: ObservableObject {
         content.title = "Medikamentenerinnerung"
         content.body = "Es ist Zeit, \(medication.name) einzunehmen."
         content.sound = UNNotificationSound.default
-        
         
         let date = Date().setTime(hour: medication.intakeTime.hour!, minute: medication.intakeTime.minute!)
         let comps = Calendar.current.dateComponents([.year, .month, .day,.hour,.minute], from: date)
@@ -76,13 +88,13 @@ class MedicationDetailViewModel: ObservableObject {
     
     func listScheduledNotifications() {
         UNUserNotificationCenter.current().getPendingNotificationRequests { notifications in
-                notifications.forEach { notification in
-                    guard let dateComponents = (notification.trigger as? UNCalendarNotificationTrigger)?.dateComponents,
-                          let date = Calendar.current.date(from: dateComponents) else { return }
-                    print("-----> Notifications: ", date, notification.identifier)
-                }
+            notifications.forEach { notification in
+                guard let dateComponents = (notification.trigger as? UNCalendarNotificationTrigger)?.dateComponents,
+                      let date = Calendar.current.date(from: dateComponents) else { return }
+                print("-----> Notifications: ", date, notification.identifier)
             }
         }
+    }
 
     /**
      Fügt ein neues Medikament hinzu und plant eine Benachrichtigung dafür.
@@ -97,26 +109,67 @@ class MedicationDetailViewModel: ObservableObject {
      - Parameter userId: Die eindeutige Kennung des Benutzers.
      */
     func addMedication(name: String, intakeTime: DateComponents, day: String, nextIntakeDate: DateComponents?, color: MedicationColor, dosage: Int, dosageUnit: DosageUnit, userId: String) {
-        let newMedicationID = UUID().uuidString
-        let newMedication = Medication(id: newMedicationID, name: name, intakeTime: intakeTime, day: day, nextIntakeDate: nil, color: color, dosage: dosage, dosageUnit: dosageUnit)
-        
-        do {
-            // Hauptmedikation speichern
-            let _ = try Firestore.firestore().collection("users").document(userId).collection("medications").document(newMedicationID).setData(from: newMedication)
-            medications.append(newMedication)// Mit addSnapshotlistener
-            scheduleNotification(for: newMedication)
-            
-            // Optionale Medikation speichern, falls vorhanden
-            if let nextIntakeDate = nextIntakeDate {
-                let nextMedicationID = UUID().uuidString
-                let nextDay = Weekday.from(nextIntakeDate.weekday)?.name ?? "Unbekannt"
-                let nextMedication = Medication(id: nextMedicationID, name: name, intakeTime: nextIntakeDate, day: nextDay, nextIntakeDate: nil, color: color, dosage: dosage, dosageUnit: dosageUnit)
-                let _ = try Firestore.firestore().collection("users").document(userId).collection("medications").document(nextMedicationID).setData(from: nextMedication)
-                medications.append(nextMedication)
-                scheduleNotification(for: nextMedication)
+        let firestore = Firestore.firestore()
+        let medicationRef = firestore.collection("users").document(userId).collection("medications").document()
+
+        // Den Snapshot-Listener für die neue Medikation setzen
+        medicationRef.addSnapshotListener { documentSnapshot, error in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
             }
+            guard let data = document.data() else {
+                print("Document data was empty.")
+                return
+            }
+
+            do {
+                let medication = try document.data(as: Medication.self)
+                self.medications.append(medication)
+                self.scheduleNotification(for: medication)
+            } catch let error {
+                print("Error deserializing medication: \(error)")
+            }
+        }
+
+        let newMedication = Medication(id: medicationRef.documentID, name: name, intakeTime: intakeTime, day: day, nextIntakeDate: nil, color: color, dosage: dosage, dosageUnit: dosageUnit)
+
+        do {
+            try medicationRef.setData(from: newMedication) // Daten in Firestore speichern
         } catch let error {
             print("Error adding medication: \(error)")
+        }
+
+        // Optionale Medikation speichern, falls vorhanden
+        if let nextIntakeDate = nextIntakeDate {
+            let nextMedicationRef = firestore.collection("users").document(userId).collection("medications").document()
+            let nextDay = Weekday.from(nextIntakeDate.weekday)?.name ?? "Unbekannt"
+            let nextMedication = Medication(id: nextMedicationRef.documentID, name: name, intakeTime: nextIntakeDate, day: nextDay, nextIntakeDate: nil, color: color, dosage: dosage, dosageUnit: dosageUnit)
+
+            nextMedicationRef.addSnapshotListener { documentSnapshot, error in
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    return
+                }
+                guard let data = document.data() else {
+                    print("Document data was empty.")
+                    return
+                }
+
+                do {
+                    let medication = try document.data(as: Medication.self)
+                    self.medications.append(medication)
+                    self.scheduleNotification(for: medication)
+                } catch let error {
+                    print("Error deserializing medication: \(error)")
+                }
+            }
+
+            do {
+                try nextMedicationRef.setData(from: nextMedication) // Daten in Firestore speichern
+            } catch let error {
+                print("Error adding medication: \(error)")
+            }
         }
     }
 
@@ -130,13 +183,16 @@ class MedicationDetailViewModel: ObservableObject {
         guard let id = medication.id else { return }
 
         Firestore.firestore().collection("users").document(userId).collection("medications").document(id).delete { error in
-            if let error = error {
+            if let error {
                 print("Error deleting medication: \(error)")
                 return
             }
 
             self.medications.removeAll { $0.id == medication.id }
         }
+        
+        // Den Snapshot-Listener für die gelöschte Medikation aktualisieren
+        fetchMedications(userId: userId)
     }
 
     /**
@@ -151,17 +207,36 @@ class MedicationDetailViewModel: ObservableObject {
             return
         }
 
-        do {
-            try Firestore.firestore().collection("users").document(userId).collection("medications").document(id).setData(from: medication)
-            if let index = self.medications.firstIndex(where: { $0.id == medication.id }) {
-                self.medications[index] = medication
-                scheduleNotification(for: medication)
-                print("Medication updated successfully")
+        let medicationRef = Firestore.firestore().collection("users").document(userId).collection("medications").document(id)
+        
+        // Den Snapshot-Listener für die aktualisierte Medikation setzen
+        medicationRef.addSnapshotListener { documentSnapshot, error in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
             }
+            guard let data = document.data() else {
+                print("Document data was empty.")
+                return
+            }
+
+            do {
+                let updatedMedication = try document.data(as: Medication.self)
+                if let index = self.medications.firstIndex(where: { $0.id == updatedMedication.id }) {
+                    self.medications[index] = updatedMedication
+                    self.scheduleNotification(for: updatedMedication)
+                    print("Medication updated successfully")
+                }
+            } catch let error {
+                print("Error deserializing medication: \(error)")
+            }
+        }
+
+        do {
+            try medicationRef.setData(from: medication) // Daten in Firestore speichern
         } catch let error {
             print("Error updating medication: \(error)")
         }
     }
 }
-
 
